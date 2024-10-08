99d61da8147b560465299c492f410fa3f2a83d2e
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-10-08 11:53:24 +0500

    123

--- CleanCode&CodeStyle9/Program.cs
+++ CleanCode&CodeStyle9/Program.cs
@@ -7,135 +7,205 @@ namespace IMJunior
     {
         static void Main(string[] args)
         {
-            OrderForm orderForm = new OrderForm();
-            PaymentHandler paymentHandler = new PaymentHandler();
+            IPaymentSystemRespository paymentSystemRespository = new PaymentSystemRespository();
+            OrderForm orderForm = new OrderForm(paymentSystemRespository.GetSystemFactoryIdNames());
 
-            string systemId = orderForm.ShowForm(paymentHandler.PaymentSystems);
+            orderForm.ShowForm();
 
-            IPaymentSystem chosenPaymentSystem = paymentHandler.GetChosenPaymentSystem(systemId);
+            string systemId = orderForm.ChoosePaymentSystemId();
+            IPaymentSystemFactory paymentSystemFactory = paymentSystemRespository.FindFactory(systemId);
+            PaymentHandler paymentHandler = new PaymentHandler(paymentSystemFactory);
 
-            paymentHandler.ShowPaymentResult(chosenPaymentSystem);
+            paymentHandler.AcceptPayment();
+            paymentHandler.ShowPaymentResult();
+        }
+    }
+
+    public static class PaymentSystemId
+    {
+        public const string Qiwi = "QIWI";
+        public const string WebMoney = "WebMoney";
+        public const string Card = "Card";
+    }
+
+    public interface IPaymentSystemRespository
+    {
+        IPaymentSystemFactory FindFactory(string systemId);
+
+        IEnumerable<string> GetSystemFactoryIdNames();
+    }
+
+    public class PaymentSystemRespository : IPaymentSystemRespository
+    {
+        private Dictionary<string, IPaymentSystemFactory> _factories;
+
+        public PaymentSystemRespository()
+        {
+            _factories = CreateFactories();
+        }
+
+        public IPaymentSystemFactory FindFactory(string systemId)
+        {
+            if (systemId == null)
+                throw new ArgumentNullException(nameof(systemId));
+
+            if (systemId.Length == 0)
+                throw new ArgumentOutOfRangeException(nameof(systemId));
+
+            systemId = FindPaymentSystemIdName(systemId);
+
+            return _factories[systemId];
+        }
+
+        public IEnumerable<string> GetSystemFactoryIdNames()
+        {
+            return _factories.Keys;
+        }
+
+        private Dictionary<string, IPaymentSystemFactory> CreateFactories()
+        {
+            return new Dictionary<string, IPaymentSystemFactory>
+            {
+                { PaymentSystemId.Qiwi, new QiwiPaymentFactory() },
+                { PaymentSystemId.WebMoney, new WebMoneyPaymentFactory() },
+                { PaymentSystemId.Card, new CardPaymentFactory() }
+            };
+        }
+
+        private string FindPaymentSystemIdName(string systemId)
+        {
+            systemId = systemId.ToLower();
+
+            foreach (string systemName in _factories.Keys)
+            {
+                if (systemName.ToLower() == systemId)
+                    return systemName;
+            }
+
+            throw new InvalidOperationException($"The '{systemId}' payment system was not found.");
         }
     }
 
     public class OrderForm
     {
-        public string ShowForm(IReadOnlyList<IPaymentSystem> paymentSystems)
+        private IEnumerable<string> _paymentSystemIds;
+
+        public OrderForm(IEnumerable<string> paymentSystemIds) => 
+            _paymentSystemIds = paymentSystemIds ?? throw new ArgumentNullException(nameof(paymentSystemIds));
+
+        public void ShowForm()
         {
             string form = "Мы принимаем: ";
 
-            foreach (IPaymentSystem paymentSystem in paymentSystems)
-                form += $"{paymentSystem.Id}, ";
+            foreach (string paymentSystemId in _paymentSystemIds)
+                form += $"{paymentSystemId}, ";
 
-            Console.WriteLine(form);
+            Console.WriteLine(form);                      
+        }
 
-            //симуляция веб интерфейса
+        public string ChoosePaymentSystemId()
+        {
             Console.WriteLine("Какое системой вы хотите совершить оплату?");
+
             return Console.ReadLine();
         }
-    }
+    }    
 
     public class PaymentHandler
     {
-        private List<IPaymentSystem> _paymentSystems = new List<IPaymentSystem>
-            {
-                new Qiwi("QIWI", "Перевод на страницу QIWI...", "Проверка платежа через QIWI..."),
-                new WebMoney("WebMoney", "Вызов API WebMoney...", "Проверка платежа через WebMoney..."),
-                new Card("Card", "Вызов API банка эмитера карты Card...", "Проверка платежа через Card...")
-            };
+        private IPaymentSystem _paymentSystem;
 
-        public IReadOnlyList<IPaymentSystem> PaymentSystems => _paymentSystems;
-
-        public IPaymentSystem GetChosenPaymentSystem(string systemId)
+        public PaymentHandler(IPaymentSystemFactory factory)
         {
-            foreach (IPaymentSystem paymentSystem in _paymentSystems)
-            {
-                if (paymentSystem.Id.ToLower() == systemId.ToLower())
-                {
-                    paymentSystem.ShowProcess(paymentSystem.TransactionBeginning);
+            if (factory == null)
+                throw new ArgumentNullException(nameof(factory));
 
-                    return paymentSystem;
-                }
-            }
-
-            throw new ArgumentNullException(nameof(systemId));
+            _paymentSystem = factory.Create();
         }
 
-        public void ShowPaymentResult(IPaymentSystem paymentSystem)
-        {
-            if (paymentSystem == null)
-                throw new ArgumentNullException(nameof(paymentSystem));
+        public void AcceptPayment() => 
+            _paymentSystem.AcceptPayment();
 
-            Console.WriteLine($"Вы оплатили с помощью {paymentSystem.Id}");
-            Console.WriteLine($"{paymentSystem.TransactionEnding}\n");
+        public void ShowPaymentResult()
+        {
+            Console.WriteLine($"Вы оплатили с помощью {_paymentSystem.Id}");
+
+            _paymentSystem.CheckPayment();
 
             Console.WriteLine("Оплата прошла успешно!");
         }
     }
 
-    public class Card : IPaymentSystem
+    public class CardPaymentFactory : IPaymentSystemFactory
     {
-        public Card(string id, string transactionBeginning, string transactionEnding)
-        {
-            Id = id;
-            TransactionBeginning = transactionBeginning;
-            TransactionEnding = transactionEnding;
-        }
-
-        public string Id { get; private set; }
-
-        public string TransactionBeginning { get; private set; }
-
-        public string TransactionEnding { get; private set; }
-
-        public void ShowProcess(string process) =>
-            Console.WriteLine(process);
+        public IPaymentSystem Create() =>
+            new CardPaymentSystem();
     }
 
-    public class WebMoney : IPaymentSystem
+    public class CardPaymentSystem : IPaymentSystem
     {
-        public WebMoney(string id, string transactionBeginning, string transactionEnding)
-        {
-            Id = id;
-            TransactionBeginning = transactionBeginning;
-            TransactionEnding = transactionEnding;
-        }
+        public CardPaymentSystem() => Id = PaymentSystemId.Card;
 
         public string Id { get; private set; }
 
-        public string TransactionBeginning { get; private set; }
+        public void AcceptPayment() =>
+            Console.WriteLine("Вызов API банка эмитера карты Card...");
 
-        public string TransactionEnding { get; private set; }
-
-        public void ShowProcess(string process) =>
-            Console.WriteLine(process);
+        public void CheckPayment() => 
+            Console.WriteLine("Проверка платежа через Card...");
     }
 
-    public class Qiwi : IPaymentSystem
+    public class WebMoneyPaymentFactory : IPaymentSystemFactory
     {
-        public Qiwi(string id, string transactionBeginning, string transactionEnding)
-        {
-            Id = id;
-            TransactionBeginning = transactionBeginning;
-            TransactionEnding = transactionEnding;
-        }
+        public IPaymentSystem Create() => 
+            new WebMoneyPaymentSystem();
+    }
+
+    public class WebMoneyPaymentSystem : IPaymentSystem
+    {
+        public WebMoneyPaymentSystem() => 
+            Id = PaymentSystemId.WebMoney;
 
         public string Id { get; private set; }
 
-        public string TransactionBeginning { get; private set; }
+        public void AcceptPayment() => 
+            Console.WriteLine("Вызов API WebMoney...");
 
-        public string TransactionEnding { get; private set; }
+        public void CheckPayment() => 
+            Console.WriteLine("Проверка платежа через WebMoney...");
+    }
 
-        public void ShowProcess(string process) =>
-            Console.WriteLine(process);
+    public class QiwiPaymentFactory : IPaymentSystemFactory
+    {
+        public IPaymentSystem Create() => 
+            new QiwiPaymentSystem();
+    }
+
+    public class QiwiPaymentSystem : IPaymentSystem
+    {
+        public QiwiPaymentSystem() => 
+            Id = PaymentSystemId.Qiwi;
+
+        public string Id { get; private set; }
+
+        public void CheckPayment() => 
+            Console.WriteLine("Проверка платежа через QIWI...");
+
+        public void AcceptPayment() => 
+            Console.WriteLine("Перевод на страницу QIWI...");
+    }    
+
+    public interface IPaymentSystemFactory
+    {
+        IPaymentSystem Create();
     }
 
     public interface IPaymentSystem
     {
         string Id { get; }
-        string TransactionBeginning { get; }
-        string TransactionEnding { get; }
 
-        void ShowProcess(string process);
+        void CheckPayment();
+
+        void AcceptPayment();
     }
 }
